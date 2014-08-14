940e804b6b1293630a9bff7e4110cf17780b0942
Dave Boling <kadowki@gmail.com>
2014-08-13 21:41:26 -0500

    Fixed jQuery and re-did formatting

--- app/models/recipe.js
+++ app/models/recipe.js
@@ -3,8 +3,8 @@
 var Mongo = require('mongodb');
 
 function Recipe(o){
-  this.name = (o.name === '') ? 'Chicken' : o.name;
-  this.photo = (o.photo === '') ? 'http://seecolombia.travel/blog/wp-content/uploads/2013/02/chicken-bus-e1360602224633.jpeg' : o.photo;
+  this.name = o.name || 'Chicken';
+  this.photo = o.photo || 'http://seecolombia.travel/blog/wp-content/uploads/2013/02/chicken-bus-e1360602224633.jpeg';
   this.ingredients =  (o.ingredients.length) ? o.ingredients.split(',') : ['Stuff',' More stuff'];
   this.directions = (o.directions === '') ? 'Eat as is' : o.directions;
   this.date = new Date();
--- app/static/js/user/main.js
+++ app/static/js/user/main.js
@@ -1,76 +1,121 @@
-'use strict';
+(function () {
+  'use strict';
 
-$(document).ready(function(){
-  var $form = $('form');
-  $('.wrapper').on('click', '.x', delRecipe);
-  $('.filter').click(filterCategory);
-  $('.ingredient').on('click', filterIngredient);
+    $(document).ready(function () {
 
-  $form.hide();
-  $form.submit(addRecipe);
+        var $form = $('form');
 
-  //Hide form
-  $('#hide').click(function(){
-    $('form').hide(1000);
-  });
-
-
-  //Show form
-  $('#show').click(function(){
-    $('form').show(1000);
-  });
+        //Events
+        //---------------------------------------------------------------------
+        //---------------------------------------------------------------------
+        $('#recipes').on('click', '.x', delRecipe); //Delete recipe
+        $('.filter').click(filterCategory); //Filter by category
+        $('#recipes').on('click', '.ingredient', filterIngredient); //Filter by ingredient
+        $('#hide').click(hideForm); //Hide form
+        $('#show').click(showForm); //Show form
 
 
-  //FILTER BY CATEGORY
-  function filterCategory(e){
-    var category = $(this).text();
-    $('.category:contains(' + category + ')').closest('.recipe').fadeIn();
-    $('.category:not(:contains(' + category + '))').closest('.recipe').fadeOut();
-
-    e.preventDefault();
-   }
+        $form.hide(); //Start form hidden
+        $form.submit(addRecipe); //Hijacked submit button
+    });
 
 
-  //FILTER BY INGREDIENTS
-  function filterIngredient(e){
-    var ingredient = $(this).text();
-    $('.ingredient').not(':contains('+ingredient+')').closest('.recipe').hide();
-    $('.ingredient :contains(' + ingredient +')').closest('.recipe').show();
-
-    e.preventDefault();
-   }
 
 
-   //ADD RECIPE
-   function addRecipe(e){
-     var data = $form.serialize(),
-     type = $form.attr('method'),
-     url  = $form.attr('action');
-     $('input, textarea').val('');
 
-     $.ajax({url:url, type:type, data: data, dataType:'html', 
-       success:function(html){
-         var $recipe = $(html);
-         $('.wrapper').append($recipe);
-         $($recipe).hide().fadeIn(1000);
-       }
-     });
-     e.preventDefault();
-   }
-   //DELETE RECIPE
-   function delRecipe(){
-      var id = $(this).closest('.recipe').attr('data-recipe-id'),
-      url = '/recipes/' + id,
-      type = 'delete';
+    //Private Functions
+    //----------------------------------------------------
+    //----------------------------------------------------
 
-      $.ajax({url:url, type:type, dataType:'json',
-        success:function(data){
-          console.log(data);
-          var $r = $('.recipe[data-recipe-id=' + id + ']');
-          $r.remove(0);
-        }
-      });
+    //Show forms
+
+
+    function hideForm() {
+        $('form').hide(1000);
     }
-});
 
 
+    //Hide Forms
+
+
+    function showForm() {
+        $('form').show(1000);
+    }
+
+
+    //FILTER BY CATEGORY
+
+
+    function filterCategory(e) {
+        var category = $(this).text();
+        $('.category:contains(' + category + ')').closest('.recipe').show();
+        $('.category:not(:contains(' + category + '))').closest('.recipe').hide();
+
+        e.preventDefault();
+    }
+
+
+    //FILTER BY INGREDIENTS
+
+
+    function filterIngredient(e) {
+        var ingredient = $(this).text();
+        $('.ingredient').not(':contains(' + ingredient + ')').closest('.recipe').hide();
+        $('.ingredient :contains(' + ingredient + ')').closest('.recipe').show();
+
+        e.preventDefault();
+    }
+
+
+    //ADD RECIPE
+
+
+    function addRecipe(e) {
+        var data = $('form').serialize(),
+            type = $('form').attr('method'),
+            url = $('form').attr('action');
+
+        $('input, textarea').val('');
+
+        $.ajax({
+            url: url,
+            type: type,
+            data: data,
+            dataType: 'html',
+            success: function (html) {
+            console.log(html);
+                var $recipe = $(html);
+                $('#recipes').prepend($recipe);
+                $recipe.hide().fadeIn(2000);
+            }
+        });
+
+        e.preventDefault();
+    }
+
+
+    //DELETE RECIPE
+
+
+    function delRecipe() {
+        var id = $(this).closest('.recipe').attr('data-recipe-id'),
+            url = '/recipes/' + id,
+            type = 'delete';
+
+        $.ajax({
+            url: url,
+            type: type,
+            dataType: 'json',
+            success: function (data) {
+                console.log(data);
+                var $r = $('.recipe[data-recipe-id=' + id + ']');
+                $r.remove(0);
+            }
+        });
+    }
+
+
+
+
+
+})();
